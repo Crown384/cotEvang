06019e3d9aa987a6ad8e88eea576607f4657d5bb
crown384 <crownstephen384@gmail.com>
2025-04-11 00:10:36 +0100

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,46 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const functions = require("firebase-functions");
+const cloudinary = require("cloudinary").v2;
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: "your_cloud_name",
+  api_key: "your_api_key",
+  api_secret: "your_api_secret"
+});
+
+// HTTPS function to get Excel files from Cloudinary
+exports.getExcelFiles = functions.https.onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.status(204).send("");
+    return;
+  }
+
+  try {
+    const result = await cloudinary.search
+      .expression('resource_type:raw AND folder:ExcelFiles')
+      .max_results(50)
+      .execute();
+
+    const excelFiles = result.resources
+      .filter(file =>
+        file.format === 'xlsx' || file.format === 'xls'
+      )
+      .map(file => ({
+        fileName: file.filename,
+        url: file.secure_url
+      }));
+
+    res.status(200).json(excelFiles);
+  } catch (error) {
+    console.error("Cloudinary fetch error:", error);
+    res.status(500).json({ error: "Failed to retrieve Excel files" });
+  }
+});

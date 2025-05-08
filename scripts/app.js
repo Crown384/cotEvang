23197c831a576d4646f83d814ae22453decb1aa0
crown384 <crownstephen384@gmail.com>
2025-05-08 23:41:55 +0100

    update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -95,7 +95,7 @@ function uploadFresherFile(userUID) {
 
 async function fetchExcelFiles() {
   try {
-    const res = await fetch('https://cot-evang.vercel.app/backend/server.js');
+    const res = await fetch('https://cotevang.onrender.com');
     const files = await res.json();
 
     const container = document.getElementById('allExcelFiles');

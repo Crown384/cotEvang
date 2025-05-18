2e29fc7620a3f9b3225dccd6ddfeef01cb147668
crown384 <crownstephen384@gmail.com>
2025-05-18 08:12:19 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -193,15 +193,14 @@ async function fetchExcelFiles() {
 
 // UI control
 const setUI = (user) => {
-  const show = user ? 'block' : 'none';
-  const hide = user ? 'none' : 'block';
-
-  userInfo.forEach(el => el.style.display = show);
-  outsidersInfo.forEach(el => el.style.display = hide);
-  //outsidersInfo.forEach((el) => el.classList.remove('hidden');
-  console.log('hello');
+  if (user) {
+    document.querySelectorAll('.logged-out').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('.logged-in').forEach(el => el.style.display = 'block');
+  } else {
+    document.querySelectorAll('.logged-out').forEach(el => el.style.display = 'block');
+    document.querySelectorAll('.logged-in').forEach(el => el.style.display = 'none');
+  }
 };
-
 // Unified auth listener
 auth.onAuthStateChanged(user => {
   if (user) {
@@ -223,7 +222,7 @@ auth.onAuthStateChanged(user => {
 
   } else {
     setUI(null);
-    console.log('nomuser')
+    console.log('')
     //M.toast({ html: `No User logged In` });
   }
 });
